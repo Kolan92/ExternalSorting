85534a37e276e6a07f8b51701d270677c3def2d3
Pawel Kolanowski <pawel.krzysztof.kolanowski@gmail.com>
2018-01-15 21:54:54 +0100

    Removed unused code

--- ExternaSorting/ExternalSorting/SortTask/IMemoryChecker.cs
+++ ExternaSorting/ExternalSorting/SortTask/IMemoryChecker.cs
@@ -7,29 +7,16 @@ namespace ExternalSorting.SortTask
         bool CanFitWholeFileToMemory(string filePath);
     }
 
-    public class MemoryData
-    {
-        public bool CanReadWholeFile { get; private set; }
-        public int AvailableMemoryMb { get; private set; }
-
-        public MemoryData(bool canReadWholeFile, int availableMemoryMb)
-        {
-            CanReadWholeFile = canReadWholeFile;
-            AvailableMemoryMb = availableMemoryMb;
-        }
-    }
-
     public class MemoryChecker : IMemoryChecker
     {
+        private double SafetyFactor = 0.4;
+
         public bool CanFitWholeFileToMemory(string filePath)
         {
-            return false;
             var fileMegabytes = new System.IO.FileInfo(filePath).Length / 1024 / 1024; //Get file size in megabytes
             var performanceCounter  = new PerformanceCounter("Memory", "Available MBytes", true);
             var availableMemory = performanceCounter.NextValue();
             return fileMegabytes < (availableMemory * SafetyFactor);
         }
-
-        private double SafetyFactor = 0.4;
     }
 }
